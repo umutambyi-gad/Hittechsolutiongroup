1a52503b29cf773cf31069623be3f6c427811735
Umutambyi Gad <umutambyig@gmail.com>
2021-02-08 22:02:54 +0100

    Configured database settings

--- Hittectsolutiongroup/settings.py
+++ Hittectsolutiongroup/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -39,6 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Homepage',
+    'Aboutpage',
+    'Servicespage',
+    'Memberspage',
+    'Blogpage',
+    'Contactpage',
+    'django_cleanup'
 ]
 
 MIDDLEWARE = [
@@ -85,11 +92,13 @@ WSGI_APPLICATION = 'Hittectsolutiongroup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -128,3 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join('/media/')
--- Hittectsolutiongroup/urls.py
+++ Hittectsolutiongroup/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Homepage.urls')),
-    path('about-us', include('Aboutpage.urls')),
-    path('services', include('Servicespage.urls')),
-    path('staff-members', include('Memberspage.urls')),
-    path('blogs', include('Blogpage.urls')),
-    path('contact', include('Contactpage.urls')),
+    path('about-us/', include('Aboutpage.urls')),
+    path('services/', include('Servicespage.urls')),
+    path('staff-members/', include('Memberspage.urls')),
+    path('blogs/', include('Blogpage.urls')),
+    path('contact/', include('Contactpage.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
