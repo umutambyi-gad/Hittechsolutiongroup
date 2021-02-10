f8f2042fb8afd2b9400c96fab4662432ad690962
Umutambyi Gad <umutambyig@gmail.com>
2021-02-10 20:13:21 +0100

    Removed forward slash after blog title(slug) in the blogpage.urls.py

--- Blogpage/urls.py
+++ Blogpage/urls.py
@@ -5,5 +5,5 @@ app_name = 'blogpage'
 
 urlpatterns = [
 	path('', blog, name='blog'),
-	path('<int:blog_id>/<slug:blog_title_slug>/', blog_single, name='blog-single'),
+	path('<int:blog_id>/<slug:blog_title_slug>', blog_single, name='blog-single'),
 ]
