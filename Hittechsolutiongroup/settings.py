605d2de063e787513e63bbd54edffcb24939b6b9
Umutambyi Gad <umutambyig@gmail.com>
2021-02-10 06:15:32 +0100

    Fixed a bug in the media an static files

--- Hittechsolutiongroup/settings.py
+++ Hittechsolutiongroup/settings.py
@@ -151,8 +151,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "assets")
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "uploads")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
