ecad0654dc733b714b64e646e878de812bb1272a
Umutambyi Gad <umutambyig@gmail.com>
2021-02-11 21:26:10 +0100

    Used dj_database_url library to parse database url in the environment variable

--- Hittechsolutiongroup/settings.py
+++ Hittechsolutiongroup/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -95,12 +96,7 @@ WSGI_APPLICATION = 'Hittechsolutiongroup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT')
+        dj_database_url.config()
     }
 }
 
