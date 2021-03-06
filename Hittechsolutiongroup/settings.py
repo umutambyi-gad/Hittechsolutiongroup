51f03b438abb7951d175f0cd2592f0d84eeddd5c
Umutambyi Gad <umutambyig@gmail.com>
2021-03-06 18:35:34 +0100

    Added admin reorder into middleware

--- Hittechsolutiongroup/settings.py
+++ Hittechsolutiongroup/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'admin_reorder.middleware.ModelAdminReorder'
 ]
 
 ROOT_URLCONF = 'Hittechsolutiongroup.urls'
